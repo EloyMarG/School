57c2205f0f10cca5d289dad42a197b3eb681e491
MARTIN GASCON Eloy <eloy.martin@soprasteria.com>
2022-04-25 14:00:07 +0200

    creado el proyecto School_5

--- School_5/wwwroot/js/site.js
+++ School_5/wwwroot/js/site.js
@@ -1,11 +1,22 @@
 ﻿
 let App = {
 
-        Core: {},
+        Core: {
+            CreateTable: function(id) {
+                $("#" + id).DataTable({
+                    "language": {
+                        "url": "//cdn.datatables.net/plug-ins/1.10.19/i18n/Spanish.json"
+                    }
+                });           
+        },
         Data: {},
         Views: {
             Estudiantes:{
-                Table: {},
+                Table: {
+                    OnLoad: function() {
+                        App.Core.CreateTable("tablaEstudiantes")
+                    }
+                },
                 File: {
                     OnLoad: function() {}
                 },
